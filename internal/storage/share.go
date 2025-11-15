2fe99e71736d6eab54e3c4854460817e8925a697
TuanKiet <155tuankiet@gmail.com>
2025-11-15 11:18:14 +0700

    sửa lại tên attribute cho khớp với DB

--- internal/storage/share.go
+++ internal/storage/share.go
@@ -60,7 +60,7 @@ func (r *postgresShareRepository) GetShareByID(ctx context.Context, shareID int6
 
 //Các hàm truy xuất DB liên quan đến authorize password 
 func (r *postgresShareRepository) GetPasswordHash(ctx context.Context, shareID int64) (string, error) {
-	const query = `SELECT passwordHash FROM shares WHERE id = $1`
+	const query = `SELECT hash_password FROM shares WHERE id = $1`
 	var passwordHash string
 	err := r.db.GetContext(ctx, &passwordHash, query, shareID)
 	if err != nil {

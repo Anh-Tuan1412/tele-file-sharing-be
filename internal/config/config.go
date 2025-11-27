5f3d3802976424b33d49939e2ca88054e92de1ed
nhanx04 <nhan.nguyencs04@gmail.com>
2025-11-28 02:01:30 +0700

    refactor config for CD

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"os"
+
 	"github.com/spf13/viper"
 )
 
@@ -15,8 +17,14 @@ func LoadConfig(path string) (config *Config, err error) {
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
 
+	// Try to read config file, but don't fail if it doesn't exist
+	// (in production on Fly.io, env vars are set directly)
 	if err = viper.ReadInConfig(); err != nil {
-		return nil, err
+		// Only fail if we're not in production (no FLY_APP_NAME env var)
+		if os.Getenv("FLY_APP_NAME") == "" {
+			return nil, err
+		}
+		// In production, continue without the config file
 	}
 
 	if err = viper.Unmarshal(&config); err != nil {
